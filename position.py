14557c23b0ecd5a973c36a5479b3837096f061b0
Felipe <feliperogai@hotmail.com>
2024-07-16 10:37:31 -0300

    update

---
+++ position.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+# Define a posição (x, y) onde o clique será realizado
+x, y = 1050, 315
+
+# Realiza um clique na posição (x, y)
+pyautogui.click(x, y)
---
+++ python auto_click.py
@@ -0,0 +1,20 @@
+import pyautogui
+import time
+
+# Define a posição onde o clique será realizado
+x, y = 1050, 315
+
+# Função para realizar o clique
+def clicar_na_posicao():
+    pyautogui.click(x, y)
+    print(f"Clique realizado na posição ({x}, {y})")
+
+try:
+    while True:
+        clicar_na_posicao()
+        # Espera 15 minutos (900 segundos) antes de realizar o próximo clique
+        time.sleep(900)
+except KeyboardInterrupt:
+    print("\nPrograma interrompido pelo usuário.")
+except Exception as e:
+    print(f"\nOcorreu um erro: {e}")
